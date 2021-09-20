3e8cff346172216addfd1072ffe2c5fb282ca5b4
Silambarasan <silambu1903@gmail.com>
2021-09-20 17:34:27 +0530

    list item

--- lib/screens/usersListScreen.dart
+++ lib/screens/usersListScreen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfirebaseapp/colors/colors.dart';
 
@@ -53,13 +54,12 @@ class _UserListScreenState extends State<UserListScreen> {
 
 Widget userListItem(BuildContext context, dynamic url) {
   return SafeArea(
-   child: ListView.builder(
-     itemCount: 6,
-     itemBuilder: (BuildContext context, int index) {
-     return Item(context);
-   },
-
-   ),
+    child: ListView.builder(
+      itemCount: 6,
+      itemBuilder: (BuildContext context, int index) {
+        return Item(context);
+      },
+    ),
   );
 }
 
@@ -214,15 +214,148 @@ Widget list(BuildContext context) {
 }
 
 Widget Item(BuildContext context) {
-  return Container(
-    margin: EdgeInsets.all(10),
-    width: MediaQuery.of(context).size.width * 0.9,
-    height: MediaQuery.of(context).size.height *0.2,
-    decoration: BoxDecoration(
-      color: MyColors.primary,
-      borderRadius: BorderRadius.all(
-        Radius.circular(10.0),
+  return Column(
+    children: [
+      Row(
+        children: [
+          Expanded(
+            flex: 5,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 0, 10),
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                color: MyColors.primary,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  topLeft: Radius.circular(10.0),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(20, 15, 10, 0),
+                        child: CircleAvatar(
+                          radius: 35.0,
+                          backgroundImage: NetworkImage(
+                              'https://firebasestorage.googleapis.com/v0/b/fluttersocialchat-73c72.appspot.com/o/images%2FsilambuprofilePic?alt=media&token=4970f93f-775d-4b50-afac-08f1a2193b9f'),
+                          backgroundColor: Colors.transparent,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(12.0, 15, 10, 0),
+                            child: Text(
+                              "silambarasan",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.025),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(10.0, 0, 10, 0),
+                            child: Text(
+                              "Mobile App developer",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.02),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                        width: MediaQuery.of(context).size.width * 0.025,
+                        height: MediaQuery.of(context).size.height * 0.01,
+                        decoration: BoxDecoration(
+                            color: Colors.green, shape: BoxShape.circle),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(1.0, 10, 0, 0),
+                        child: Text(
+                          "online",
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontSize:
+                                  MediaQuery.of(context).size.height * 0.02),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 10, 10, 10),
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                color: MyColors.secondary,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 0, 10, 0),
+                    child: Text(
+                      "9.2",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.025),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 2, 10, 0),
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.white, width: 3.0),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 10, 10, 0),
+                    child: Text(
+                      "12345",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.015),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 2, 10, 0),
+                    child: Text(
+                      "reviews",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.013),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-    ),
+    ],
   );
 }
